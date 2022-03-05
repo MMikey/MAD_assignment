0eaea15e8288b768481781d15213b986e6e0cde0
= <michael.mulgrew@hotmail.com>
2022-03-05 14:52:18 +0000

    updated app.js

--- assignment/App.js
+++ assignment/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const HelloWorldApp = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
-  )
+class SayHello extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Hello {this.props.name} </Text>
+      </View>
+    )
+  };
 }
+
+class HelloWorldApp extends Component {
+  render(){ 
+    let name = 'Mikey'
+
+    return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+          <SayHello name="Mike"/>
+        </View>
+      )
+    };
+}
+
+
 export default HelloWorldApp;
